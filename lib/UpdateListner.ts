8ead09af167876f5dd8a2034d6cbcf88b0eb6273
Jeroen Aartse <jeroen@webdesigntilburg.nl>
2024-05-16 21:03:34 +0200

    finished for publish

--- lib/UpdateListner.ts
+++ lib/UpdateListner.ts
@@ -5,42 +5,73 @@ import DucoApi from './api/DucoApi';
 import NodeInterface from './types/NodeInterface';
 import NodeHelper from './NodeHelper';
 import DucoDriver from './homey/DucoDriver';
+import { Driver } from 'homey';
+import { Device } from 'homey/lib/FlowCardTriggerDevice';
 
 export default class UpdateListener {
 
     refreshInterval: number = 60000
     ducoApi: DucoApi
     homey: Homey
+    timeoutId: any
 
     constructor(homey: Homey) {
         this.ducoApi = new DucoApi(homey);
         this.homey = homey;
+        this.timeoutId = null;
+
+        const timeoutCallback = () => {
+            this.startListener();
+        }
+
+        const onSettingsChange = (field: any) => {
+            if ('hostname' === field) {
+                // stop the listener interval
+                if (this.timeoutId) {
+                    this.homey.clearInterval(this.timeoutId);
+                    this.timeoutId = null;
+                }
+
+                // restart listener with a timeout to make sure the hostname is changed
+                this.homey.setTimeout(timeoutCallback, 1000);
+            }
+        }
+
+        this.homey.settings.on('set', onSettingsChange);
     }
 
     startListener() {
+        // use polling to update the data
         const timeoutCallback = () => {
             this.updateDevices();
         }
+        this.timeoutId = this.homey.setInterval(timeoutCallback, this.refreshInterval);
 
-        this.homey.setTimeout(timeoutCallback, this.refreshInterval);
+        // update devices when start listener
+        this.updateDevices();
     }
 
     updateDevices() {
         this.ducoApi.getNodes().then(nodes => {
             nodes.forEach((node: NodeInterface) => {
-                this.updateDeviceByNode(node);
+                this.updateDriverByNode(node);
             });
+        }).catch((err) => {
+            const drivers = this.homey.drivers.getDrivers();
+            for(const id in drivers) {
+                const driver = <DucoDriver>drivers[id];
 
-            this.startListener();
-        }).catch(() => {
-            this.startListener();
+                driver.setUnavailable(err);
+            }
         });
     }
 
-    updateDeviceByNode(node: NodeInterface) {
+    updateDriverByNode(node: NodeInterface) {
         NodeHelper.getDriverIdsForNodeType(node.General.Type.Val).forEach((driverId: string) => {
-            const driver = this.homey.drivers.getDriver(driverId);
-            (<DucoDriver>driver).updateByNode(node);
+            const driver = <DucoDriver>this.homey.drivers.getDriver(driverId);
+            
+            driver.setAvailable();
+            driver.updateByNode(node);
         })
     }
 }
--- lib/homey/DucoDriver.ts
+++ lib/homey/DucoDriver.ts
@@ -5,7 +5,20 @@ import DucoDevice from "./DucoDevice";
 export default class DucoDriver extends Homey.Driver {
     updateByNode(node: NodeInterface) {
       this.getDevices().forEach((device: Device) => {
+        device.setAvailable();
         (<DucoDevice>device).updateByNode(node);
+      })
+    }
+
+    setUnavailable(message?: string | null | undefined) {
+      this.getDevices().forEach((device: Device) => {
+        device.setUnavailable(message);
+      })
+    }
+
+    setAvailable() {
+      this.getDevices().forEach((device: Device) => {
+        device.setAvailable();
       })
     }
 }
